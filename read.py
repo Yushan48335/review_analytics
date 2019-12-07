b9160c13bcd6b3e15c85ad35895c7a1fa16f8ad2
Yushan <yushan48335@gmail.com>
2019-12-07 14:14:50 +0800

    calculate average

--- read.py
+++ read.py
@@ -1,9 +1,13 @@
 data = []
 count = 0
+total_len = 0
 with open('reviews.txt', 'r') as r:
 	for line in r:
 		data.append(line)
+		total_len += len(line)
 		count += 1
 		if count % 10000 == 0:
 			print(len(data))
-print('Total: ', len(data))
+ave_len = total_len / len(data)
+print('Total: ', len(data), ', average for every line is: ', ave_len)
+
